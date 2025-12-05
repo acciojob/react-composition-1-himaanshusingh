e14b2359b0d757d0c6da3a5d4cfaaa6eebdc7ba8
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-05 21:59:19 +0530

    Correct the code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
 import React from "react";
-import './../styles/App.css';
+import Tabs from "./Tabs";
+
+const tabsData = [
+  { title: "Tab 1", content: "This is the content for Tab1" },
+  { title: "Tab 2", content: "This is the content for Tab2" },
+  { title: "Tab 3", content: "This is the content for Tab3" },
+];
 
 const App = () => {
   return (
     <div>
-        <h1>Hello World</h1>
+      <h1>Tabs Demo</h1>
+      <Tabs tabs={tabsData} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
