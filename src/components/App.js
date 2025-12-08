164af74e938361a725a31e9c4d50ce7792d17a60
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-08 23:39:23 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import Tabs from "./Tabs";
 
 const App = () => {
   const tabsData = [
-    { title: "Tab 1", content: "This is the content for Tab1" },
-    { title: "Tab 2", content: "This is the content for Tab2" },
-    { title: "Tab 3", content: "This is the content for Tab3" },
+    { title: "Tab 1", content: "This is the content for Tab 1" },
+    { title: "Tab 2", content: "This is the content for Tab 2" },
+    { title: "Tab 3", content: "This is the content for Tab 3" },
   ];
 
   return (
