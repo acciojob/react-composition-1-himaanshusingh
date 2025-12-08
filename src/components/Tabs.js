b0c1236d9516a5211095cb32a910b96d8ea35dfb
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-08 23:47:53 +0530

    commit

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -7,12 +7,18 @@ const Tabs = ({ tabs }) => {
     <div className="tabs-container">
       <ul className="tabs">
         {tabs.map((tab, index) => (
-          <li key={index} className={activeTab === index ? "active" : ""} onClick={() => setActiveTab(index)}>{tab.title}</li>
+          <li
+            key={index}
+            className={activeTab === index ? "active" : ""}
+            onClick={() => setActiveTab(index)}
+          >
+            {tab.title}
+          </li>
         ))}
       </ul>
-      <div className="tab-content">{tabs[activeTab] && tabs[activeTab].content}</div>
-    </div> 
-  ); // prettier-ignore
+      <div className="tab-content">{tabs[activeTab]?.content || ""}</div>
+    </div>
+  );
 };
 
 export default Tabs;
